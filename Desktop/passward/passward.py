f476477b05bc2509a211b6f40af8ac4d1a3bc5a5
Jimmy Lo <jimmylo900803@gmail.com>
2020-08-17 17:21:43 +0800

    fixed

--- Desktop/passward/passward.py
+++ Desktop/passward/passward.py
@@ -1,10 +1,12 @@
-x = 0
+x = 3
 passward = "jimmylo123"
-while x < 3:
+while x > 0:
+	x = x - 1
 	pwd = input("請輸入密碼")
 	if pwd == passward:
 		print ("登入成功")
 		break
 	else:
-		x = x + 1
-		print ("登入失敗 還有", 3 - x ,"次機會")
+		print ("登入失敗")
+		if x > 0:
+			print (" 還有", x ,"次機會")
